924afd9843a24f39a039b2f7cf8adf936704b03d
mosttafa9314reg <mostafa9314reg@gmail.com>
2025-08-13 00:28:00 +0330

    adding put delete method in fastapi and it still amazing

--- Books.py
+++ Books.py
@@ -28,6 +28,14 @@ async def  read_author_category(category:str):
     return books_to_return
 
 
+@app.get("/books/by_author")
+async  def read_books_by_author(Author:str):
+    books_to_return = []
+    for book in BOOKS :
+        if book.get('Author').casefold() == Author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
 
 @app.get("/books/{book_author}/")
 async def  read_author_category(category:str,book_author:str):
@@ -42,3 +50,16 @@ async def  read_author_category(category:str,book_author:str):
 @app.post("/books/create_book")
 async def creat_new_book(new_book=Body()):
     BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('Title').casefold() == updated_book.get('Title').casefold():
+            BOOKS[i] = updated_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title : str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('Title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
