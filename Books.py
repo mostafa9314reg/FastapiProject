8f0028083b3eb0149072737a3202e6ce7e464737
mosttafa9314reg <mostafa9314reg@gmail.com>
2025-08-12 22:35:18 +0330

    adding method with query_path and auery_parametr

--- Books.py
+++ Books.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 BOOKS= [{"Title":"book1","Author":"Author1","Category":"Category1"},
-        {"Title":"book2","Author":"Author2","Category":"Category2"},
+        {"Title":"book2","Author":"Author2","Category":"Category1"},
         {"Title":"book3","Author":"Author3","Category":"Category3"},
         {"Title":"book4","Author":"Author4","Category":"Category4"},
 ]
@@ -19,4 +19,21 @@ async def read_book(book_title:str):
         if book.get('Title').casefold() == book_title.casefold():
             return book
 
+@app.get("/books/")
+async def  read_author_category(category:str):
+    books_to_return =[]
+    for book in BOOKS:
+        if book.get('Category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
 
+
+
+@app.get("/books/{book_author}/")
+async def  read_author_category(category:str,book_author:str):
+    books_to_return =[]
+    for book in BOOKS:
+        if book.get('Category').casefold() == category.casefold() and \
+            book.get('Author').casefold() == book_author.casefold():
+            books_to_return.append(book)
+    return books_to_return
