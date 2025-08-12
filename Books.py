0bab11bc5cf8ec3b15aa11fd9e9d89620926ab44
mosttafa9314reg <mostafa9314reg@gmail.com>
2025-08-12 21:36:36 +0330

    add read_book method

--- Books.py
+++ Books.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-BOOKS= {"Title":"book1","Author":"Author1","Category":"Category1"}
-
+BOOKS= [{"Title":"book1","Author":"Author1","Category":"Category1"},
+        {"Title":"book2","Author":"Author2","Category":"Category2"},
+        {"Title":"book3","Author":"Author3","Category":"Category3"},
+        {"Title":"book4","Author":"Author4","Category":"Category4"},
+]
 
 @app.get("/allbooks")
-async def first_api():
+async def read_aLL_books():
     #return  {"message":"This is our first fastapi try"}
     return BOOKS
+
+@app.get("/book/{book_title}")
+async def read_book(book_title:str):
+    for book in BOOKS:
+        if book.get('Title').casefold() == book_title.casefold():
+            return book
+
+
